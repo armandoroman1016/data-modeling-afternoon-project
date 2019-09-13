42d5e4bc3cfacba84b38ca510cec750fcdc69875
Armando Roman <armandoroman1016@gmail.com>
2019-09-12 18:30:01 -0700

    get shopping list complete

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express')
-const ingredientsRoute = require('./routes/ingredients-routes')
+const ingredientsRouter = require('./routes/ingredients-routes')
+const recipesRouter = require('./routes/recipes-routes')
 
 const server = express()
 
 server.use(express.json())
-server.use('/api/ingredients', ingredientsRoute)
+server.use('/api/ingredients', ingredientsRouter)
+server.use('/api/recipes', recipesRouter)
+
 
 module.exports = server
 
